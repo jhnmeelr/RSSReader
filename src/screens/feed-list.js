87091ddb7ae6ed9e174b3bdb8d8a859a470f8797
jm <jhn.meelr@gmail.com>
2017-12-07 22:31:10 +0200

    Add: event handlers for feed list

--- src/screens/feed-list.js
+++ src/screens/feed-list.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { Container, Content, List, ListItem, Text } from 'native-base';
+import { Container, Content, List, ListItem, Text, Icon, Button } from 'native-base';
 import { observer } from 'mobx-react/native';
+import { selectFeed } from '../actions';
 
 @observer
 export default class FeedsList extends React.Component {
+  static navigationOptions = (props) => ({
+    title: 'My Feeds',
+    headerRight: <Button transparentonPress={ () => props.navigation.navigate('AddFeed') }>
+                   <Icon name="add"/>
+                 </Button>
+  })
+
+  handleFeedPress(feed) {
+    selectFeed(feed);
+    this.props.navigation.navigate('FeedDetail', { feedUrl: feed.url });
+  }
+
   render() {
     const { feeds } = this.props.screenProps.store;
-
     return (
       <Container>
-        <Content>
-          <List>
-            {feeds && feeds.map((f, i) => (
-              <ListItem key={i}>
-                <Text>{f.title}</Text>
-              </ListItem>
-            ))}
-          </List>
-        </Content>
+          <Content>
+              <List>
+                  {
+                    feeds && feeds.map((f, i) => {
+                      return (
+                        <ListItem key={i} onPress={() => this.handleFeedPress(f)}>
+                          <Text>{f.title}</Text>
+                        </ListItem>
+                      );
+                    })
+                  }
+              </List>
+          </Content>
       </Container>
     );
   }
